70235f396eb0da7a465a4bbb4067f0bae6574d38
Sarah Hoffmann <lonvia@denofr.de>
2019-12-29 00:00:50 +0100

    remove useless cmake files

--- release-docs/3.1.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/docs/Nominatim-3.1.0/docs
-# Build directory: /tmp/docs/Nominatim-3.1.0/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
